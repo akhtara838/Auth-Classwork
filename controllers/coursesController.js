ba94269e6d27bb988cf6336802e19a21b89e1e92
Ayesha Akhtar <ayeshaakhtar13@gmail.com>
2021-04-12 22:09:57 -0600

    working

---
+++ controllers/coursesController.js
@@ -0,0 +1,103 @@
+"use strict";
+const Course = require("../models/course");
+
+module.exports = {
+    index: (req, res, next) => {
+        Course.find()
+            .then(courses => {
+                res.locals.courses = courses;
+                next();
+            })
+            .catch(error => {
+                console.log(`Error fetching course data: ${error.message}`)
+                next(error);
+            })
+    },
+    indexView: (req, res) => {
+        res.render("courses/index");
+    },
+    new: (req, res) => {
+        res.render("courses/new");
+    },
+    create: (req, res, next) => {
+        let newCourse = new Course({
+            title: req.body.title,
+            description: req.body.description,
+            maxStudents: req.body.maxStudents,
+            cost: req.body.cost
+        });
+        Course.create(newCourse)
+            .then(course => {
+                res.locals.course = course;
+                res.locals.redirect = "/courses";
+                next();
+            })
+            .catch(error => {
+                console.log(`Error saving user: ${error.message}`);
+                next(error)
+            })
+    },
+    redirectView: (req, res, next) => {
+        let redirectPath = res.locals.redirect;
+        if (redirectPath != undefined) res.redirect(redirectPath);
+        else next();
+    },
+    show: (req, res, next) => {
+        let courseID = req.params.id;
+        Course.findById(courseID)
+            .then(course => {
+                res.locals.course = course;
+                next();
+            })
+            .catch(error => {
+                console.log(`Error fetching data by ID: ${error.message}`)
+                next(error);
+            })
+    },
+    showView: (req, res) => {
+        res.render("courses/show");
+    },
+    edit: (req, res, next) => {
+        let courseID = req.params.id;
+        Course.findById(courseID)
+            .then(course => {
+                res.render("courses/edit", { course: course });
+            })
+            .catch(error => {
+                console.log(`Error loading data by ID: ${error.message}`)
+                next(error);
+            })
+    },
+    update: (req, res, next) => {
+        let courseID = req.params.id;
+        let updatedCourse = new Course({
+            title: req.body.title,
+            description: req.body.description,
+            maxStudents: req.body.maxStudents,
+            cost: req.body.cost,
+            _id: courseID
+        });
+        Course.findByIdAndUpdate(courseID, updatedCourse)
+            .then(course => {
+                res.locals.course = course;
+                res.local.redirect = `/courses/${course._id}`;
+                next();
+            })
+            .catch(error => {
+                console.log(`Error fetching data by ID: ${error.message}`)
+                next(error);
+            })
+    },
+    delete: (req, res, next) => {
+        let courseID = req.params.id;
+        Course.findByIdAndRemove(courseID)
+            .then(() => {
+                res.locals.redirectView = "/courses";
+                next();
+            })
+            .catch(error => {
+                console.log(`Error fetching data by ID: ${error.message}`)
+                next(error);
+            })
+    }
+}
